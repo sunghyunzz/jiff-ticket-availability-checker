969805d874a5dc242e67780722a1a928dbad1ad7
Sunghyun Hwang <me@sunghyunzz.com>
2018-04-22 22:11:22 +0900

    Add Slack webhook options
    Signed-off-by: Sunghyun Hwang <me@sunghyunzz.com>

--- jiff/__main__.py
+++ jiff/__main__.py
@@ -1,23 +1,48 @@
 from datetime import datetime
+from typing import Optional
 
 import click
 
-import jiff
+from jiff import get_nr_of_seats, get_schedule
+from jiff.slack import send_msg
 
 
 @click.command()
 @click.argument('code')
 @click.argument('date')
-def fetch(code: str, date: str) -> None:
+@click.option('--slack-url', help='Your Slack incoming webhook URL.')
+@click.option('--slack-channel', help='Your Slack channel for webhook.')
+def fetch(
+    code: str,
+    date: str,
+    slack_url: Optional[str],
+    slack_channel: Optional[str]
+) -> None:
     try:
         sch_date = datetime.strptime(date, '%Y-%m-%d').date()
-        sch = jiff.get_schedule(
+        sch = get_schedule(
             code,
             sch_date
         )
         if sch:
-            left, total = jiff.get_nr_of_seats(sch.id)
-            click.echo(f'<{sch.title}>({code}): {left} / {total}')
+            left, total = get_nr_of_seats(sch.id)
+            msg = f'<{sch.title}>({code}): {left} / {total}'
+
+            click.echo(msg)
+            if slack_url and slack_channel:
+                sent = send_msg(
+                    slack_url,
+                    slack_channel,
+                    msg
+                )
+                if sent:
+                    click.echo('Just sent Slack message!')
+                else:
+                    click.echo('Sending Slack message failed!')
+            elif slack_url and not slack_channel:
+                click.echo('Slack URL is provided but channel is not given.')
+            elif not slack_url and slack_channel:
+                click.echo('Slack channel is provided but URL is not given.')
         else:
             click.echo('Schedule code or date are incorrect.')
     except ValueError:
